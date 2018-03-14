e132fa0d1124006440cf43dbf197e50808d784c0
Ozer Ozkahraman <ozkahramanozer@gmail.com>
2018-03-14 11:42:42 +0100

    Separated line publisher and controllers. Separated the example to its own file too.

---
+++ basic_line_follower/example_line_follower.py
@@ -0,0 +1,47 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8
+#
+# Author: Ozer Ozkahraman (ozkahramanozer@gmail.com)
+# Date: 2018-03-14
+
+import numpy as np
+
+import config
+import rospy
+import time
+
+from csv_line_publisher import LinePublisher
+from line_controller import LineController
+
+if __name__=='__main__':
+    rospy.init_node('example_line_follower', anonymous=True)
+
+    rate = rospy.Rate(config.UPDATE_FREQ)
+    waypoint_file = config.WAYPOINTS_FILE
+    pose_topic = config.POSE_TOPIC
+    line_topic = config.LINE_TOPIC
+
+
+    # parse the csv file to a list of tuples
+    with open(waypoint_file, 'r') as fin:
+        wps = [tuple(map(lambda a: float(a.strip()), line.split(','))) for line in fin.readlines()]
+
+    # line planner acts like an external planning node and publishes a line
+    # for the control to follow
+    lp = LinePublisher(waypoints = wps,
+                       pose_topic = pose_topic,
+                       line_topic = line_topic)
+
+    # controller subs to a line topic and follows that line using PID for pitch/yaw
+    # LoLo rolls when yaw'ing and we can do nothing about it now. (16/02)
+    lc = LineController(line_topic = line_topic,
+                        pose_topic = pose_topic)
+
+    t1 = time.time()
+    while not rospy.is_shutdown():
+        dt = time.time()-t1
+        lp.update()
+        lc.update(dt)
+        t1 = time.time()
+        rate.sleep()
--- basic_line_follower/line_controller.py
+++ basic_line_follower/line_controller.py
@@ -9,11 +9,10 @@ import numpy as np
 
 import time
 import sys
-
 import rospy
-from nav_msgs.msg import Path
+
 from nav_msgs.msg import Odometry
-from geometry_msgs.msg import PoseStamped
+from nav_msgs.msg import Path
 from uuv_gazebo_ros_plugins_msgs.msg import FloatStamped
 
 from utils import geometry as G
@@ -21,83 +20,6 @@ from utils import Pid
 
 import config
 
-class LinePlanner:
-    def __init__(self,
-                 waypoints,
-                 pose_topic,
-                 line_topic):
-        """
-        a very simple line follower 'planner'.
-        here just as a scaffolding that can publish Path messages we agreed on before
-        """
-
-        self._wps = waypoints
-        self._current_line = [self._wps[0], self._wps[1]]
-
-        self.pos = [0,0,0]
-        self.ori = [0,0,0,0]
-
-        # get the pose
-        rospy.Subscriber(pose_topic, Odometry, self.update_pose)
-
-        # give out a line for the controller to follow
-        self.publisher = rospy.Publisher(line_topic, Path, queue_size=10)
-
-        self._frame_id = 'world'
-
-
-    def update_pose(self, data):
-        datapos = data.pose.pose.position
-        x = datapos.x
-        y = datapos.y
-        z = datapos.z
-
-        self.pos = [x,y,z]
-
-        dataori = data.pose.pose.orientation
-        x = dataori.x
-        y = dataori.y
-        z = dataori.z
-        w = dataori.w
-
-        self.ori = [x,y,z,w]
-
-        #  self._frame_id = data.header.frame_id
-
-    def update(self):
-        xy_dist = G.euclid_distance(self.pos[:2], self._current_line[1][:2])
-        z_dist = np.abs(self.pos[2] - self._current_line[1][2])
-        if xy_dist <= config.LINE_END_XY_THRESHOLD and z_dist <= config.LINE_END_Z_THRESHOLD:
-            print('LINE CHANGED')
-            # reached the target, request to follow the next line
-            self._wps = self._wps[1:]
-            try:
-                self._current_line = [self._wps[0], self._wps[1]]
-            except IndexError:
-                print('Out of waypoints!')
-                sys.exit()
-
-        # create a Path message with whatever frame we received the localisation in
-        path = Path()
-        path.header.frame_id = self._frame_id
-
-        # Path.poses is a PoseStamped list
-        # so we have to create these objects
-        ps1 = PoseStamped()
-        ps1.header.frame_id = self._frame_id
-        ps1.pose.position.x = self._current_line[0][0]
-        ps1.pose.position.y = self._current_line[0][1]
-        ps1.pose.position.z = self._current_line[0][2]
-        path.poses.append(ps1)
-
-        ps2 = PoseStamped()
-        ps2.header.frame_id = self._frame_id
-        ps2.pose.position.x = self._current_line[1][0]
-        ps2.pose.position.y = self._current_line[1][1]
-        ps2.pose.position.z = self._current_line[1][2]
-        path.poses.append(ps2)
-
-        self.publisher.publish(path)
 
 class LoloPublisher:
     def __init__(self, frame_id='odom'):
@@ -116,6 +38,8 @@ class LoloPublisher:
         self.fin2pub = rospy.Publisher(config.LOLO_FIN2_INPUT, FloatStamped, queue_size=1)
         self.fin3pub = rospy.Publisher(config.LOLO_FIN3_INPUT, FloatStamped, queue_size=1)
         self.backfinspub = rospy.Publisher(config.LOLO_BACKFIN_INPUT, FloatStamped, queue_size=1)
+
+        self.frame_id = frame_id
 
 
     def yaw(self, direction, frame_id='odom'):
@@ -140,11 +64,11 @@ class LoloPublisher:
         self.fin3pub.publish(out)
 
         if np.sign(out.data)==1:
-            print('>>>',out.data)
+            config.pprint('>>>',out.data)
         elif np.sign(out.data)==-1:
-            print('<<<',out.data)
+            config.pprint('<<<',out.data)
         else:
-            print('---',out.data)
+            config.pprint('---',out.data)
 
     def pitch(self,direction, frame_id='odom'):
         """
@@ -157,15 +81,15 @@ class LoloPublisher:
 
         self.backfinspub.publish(out)
         if np.sign(out.data)==-1:
-            print('^^^',out.data)
+            config.pprint('^^^',out.data)
         elif np.sign(out.data)==1:
-            print('vvv',out.data)
+            config.pprint('vvv',out.data)
         else:
-            print('---',out.data)
+            config.pprint('---',out.data)
 
 
 class LineController:
-    def __init__(self, line_topic, pose_topic, lolopub):
+    def __init__(self, line_topic, pose_topic):
 
         # receive the line requests from ros
         rospy.Subscriber(line_topic, Path, self.update_line)
@@ -182,7 +106,8 @@ class LineController:
         self._yaw_pid = Pid.PID(*config.LOLO_YAW_PID)
         self._pitch_pid = Pid.PID(*config.LOLO_PITCH_PID)
 
-        self._lolopub = lolopub
+        # create a publisher for LOLO
+        self._lolopub = LoloPublisher()
 
         # we need to change from yz to xz if the two points have the same 
         # y,z values, this leads to nans in distance calculations!
@@ -280,35 +205,3 @@ class LineController:
 
 
 
-if __name__=='__main__':
-    rospy.init_node('line_follower', anonymous=True)
-
-    rate = rospy.Rate(config.UPDATE_FREQ)
-    waypoint_file = config.WAYPOINTS_FILE
-    pose_topic = config.POSE_TOPIC
-    line_topic = config.LINE_TOPIC
-
-
-    # parse the csv file to a list of tuples
-    with open(waypoint_file, 'r') as fin:
-        wps = [tuple(map(lambda a: float(a.strip()), line.split(','))) for line in fin.readlines()]
-
-    # line planner acts like an external planning node and publishes a line
-    # for the control to follow
-    lp = LinePlanner(waypoints = wps,
-                     pose_topic = pose_topic,
-                     line_topic = line_topic)
-
-    # controller subs to a line topic and follows that line using PID for pitch/yaw
-    # LoLo rolls when yaw'ing and we can do nothing about it now. (16/02)
-    lc = LineController(line_topic = line_topic,
-                        pose_topic = pose_topic,
-                        lolopub = LoloPublisher())
-
-    t1 = time.time()
-    while not rospy.is_shutdown():
-        dt = time.time()-t1
-        lp.update()
-        lc.update(dt)
-        t1 = time.time()
-        rate.sleep()
